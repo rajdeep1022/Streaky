7256b5f6c0e9e2f8b149ab8b74d9fc24aafed9cb
Suchismita-1022 <suchismita.mukhopadhyat@gmail.com>
2026-02-28 15:12:37 +0530

    add the changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
 import HabitCard from "./components/HabitCard";
+import { ComplexNavbar } from "./components/NavBar";
 
 function App() {
   return (
     <div style={{ maxWidth: "500px", margin: "auto" }}>
+      <ComplexNavbar />
       <Header />
       <HabitCard name="📖 Read Book" />
       <HabitCard name="🏋️ Go to Gym" />
